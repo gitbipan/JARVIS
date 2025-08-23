a4c3ebe8e3e130a90d29092aa268fa29a917d521
LedBipan <shresthabro.77.f@gmail.com>
2025-08-23 09:00:02 +0545

    first commit

---
+++ hello.py
@@ -0,0 +1,8 @@
+a=int(input("Enter a number"))
+b=int(input("Enter another number"))
+if(a>b):
+    print(f"{a} is the greatest")
+elif(a==b):
+    print("Numbers are equal")
+else:
+    print(f"{b} is greatest")
