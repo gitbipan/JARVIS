ebe26767e9ebddde17b8ca831b6e17963bb5e428
LedBipan <shresthabro.77.f@gmail.com>
2025-08-25 20:36:26 +0545

    fixed bug of len inside func and added restriction

--- Pw_generator.py
+++ Pw_generator.py
@@ -1,15 +1,17 @@
 import random as ran 
-def pwgen():
-    chars="""abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&{*()-_=+[]}|;:'",.<>?/`~"""
-    len=int(input("ENTER PASSWORD LENGTH"))
-    password=""
-    for a in range (len):
-        password+=ran.choice(chars)
-    return password
 while True:
+    len=int(input("ENTER PASSWORD LENGTH"))
+    def pwgen():
+        chars="""abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&{*()-_=+[]}|;:'",.<>?/`~"""
+        password=""
+        for a in range (len):
+            password+=ran.choice(chars)
+        return password
+
     pwd=pwgen()
     while pwd[0] not in "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ":
         pwd=pwgen()
     print(pwd)    
 
-     
+    
+    
